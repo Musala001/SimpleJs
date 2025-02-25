6aae9de90fb21aa2a8a945ebcada495a6e5e845e
Musala Ndouvhada <2686346@students.wits.ac.za>
2025-02-26 01:44:53 +0200

    new code

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let y = document.getElementById("btn");
+let txt = document.getElementById("txt");
+let email = document.getElementById("email");
+let btn = document.getElementById("btn");
+let textarea = document.getElementById("msg");
+let para = document.getElementById("wat");
 
-y.addEventListener("click",function(){
-    alert("Hello, welcome to my page")
+
+
+btn.addEventListener("click",function(event){
+    event.preventDefault();
+    let name = txt.value;
+    let emai = email.value;
+    let mes = textarea.value;
+    para.textContent = `Thank you, ${name}! We will reach out to you at ${emai} ur message was ${mes}`;
 });
